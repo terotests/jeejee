9d17f8080c2888f4908facfaa49296b8d7fa7f4f
Tero Tolonen <digikansio@gmail.com>
2015-09-29 15:22:16 +0300

    "commit message"

--- release/_e-0.46.js
+++ release/_e-0.46.js
@@ -6046,18 +6046,17 @@
           // if there is attributes set for the object
           baseData = _data(JSON.parse(JSON.stringify(customElem.data)));
           if (baseData) elem._compBaseData = baseData;
-        } else {
-          baseData = elem._compBaseData;
-        }
-
-        if (this.isObject(attrObj)) {
-          var oo = attrObj;
-          // TODO: make this batter, now only one-dimensional :/
-          for (var n in oo) {
-            if (oo.hasOwnProperty(n)) {
-              elem.attr(n, oo[n]);
+          if (this.isObject(attrObj)) {
+            var oo = attrObj;
+            // TODO: make this batter, now only one-dimensional :/
+            for (var n in oo) {
+              if (oo.hasOwnProperty(n)) {
+                elem.attr(n, oo[n]);
+              }
             }
           }
+        } else {
+          baseData = elem._compBaseData;
         }
 
         if (customElem.baseCss) {
